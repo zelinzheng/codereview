10f0ded13ba1bf3480b5f272189de5cb59e0e053
Cj <cj@Cjs-MacBook-Pro.local>
2018-05-17 14:11:34 -0700

    modify algorithm

--- items/amazon_models.py
+++ items/amazon_models.py
@@ -14,7 +14,7 @@ class Item(object):
         self.hotscore = 90
         self.image    = ""
 
-    def get_items(self,q_word=None):
+    def get_rectangle_items(self,q_word=None):
 
         item_list = []
 
@@ -43,6 +43,13 @@ class Item(object):
                     all_a = li.find_all('a')
                     rating_div = li.find('div', class_='a-column a-span5 a-span-last')
 
+                    if not rating_div:
+                        rating_div = li.find('div', class_='a-row a-spacing-top-mini a-spacing-none')
+
+                    if not rating_div:
+
+                        rating_div = li.find('div', class_='a-row a-spacing-none')
+
                     try:
                         rating_count = int(rating_div.find_all('a')[1].text)
                         rating = float(rating_div.find('i').text.split(" ")[0])
@@ -50,7 +57,7 @@ class Item(object):
                         link = all_a[1]['href']
                         img = all_a[0].find('img')['src']
 
-                        if title and 'https' in link and not "Learn more about Sponsored Products." in title and len(
+                        if 'https' in link and not "Learn more about Sponsored Products." in title and len(
                                 title) > 5:
                             new_item = Item()
                             new_item.title = title
@@ -68,4 +75,85 @@ class Item(object):
                 pass
 
         return sorted(item_list, key=lambda x: x.rating_count, reverse=True)
+
+    # def get_square_items(self,q_word=None):
+    #     print("sqaure items ")
+    #     item_list = []
+    #     start_time = time.time()
+    #     headers = {
+    #         'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36'}
+    #
+    #     for page in range(1, 3):
+    #
+    #         pre_url = 'https://www.amazon.com/s?url=search-alias%3Daps'
+    #         keyword_url = '&field-keywords=%s' % q_word
+    #         url = pre_url + keyword_url + '&page={0}'.format(page)
+    #
+    #         r = requests.get(url, headers=headers, timeout=5)
+    #
+    #         if int(r.status_code) == 200:
+    #             print("looks great")
+    #
+    #             soup = BeautifulSoup(r.content, "html.parser")
+    #             try:
+    #
+    #                 ul = soup.find('div', {'id': "resultsCol"})
+    #
+    #                 all_li = ul.find_all('li', class_='s-result-item')
+    #
+    #                 for li in all_li:
+    #                     all_a = li.find_all('a')
+    #                     rating_div = li.find_all('div', class_= "a-row a-spacing-none")
+    #                     if not rating_div:
+    #                         rating_div = li.find_all('div', class_="a-row a-spacing-none")
+    #                         if not rating_div:
+    #                             rating_div = li.find_all('div',class_='a-row a-spacing-top-mini')
+    #
+    #                     try:
+    #                         rating_count = int(rating_div.find_all('a')[1].text)
+    #                         print(rating_count)
+    #
+    #
+    #
+    #                     except:
+    #                         pass
+    #             except:
+    #                 pass
+    #
+    #
+    #
+    #
+    #
+    #                 # try:
+    #                 #     # rating_count = int(rating_div.find_all('a')[1].text)
+    #                 #     # rating = float(rating_div.find('i').text.split(" ")[0])
+    #                 #     title = all_a[1].text.strip()
+    #                 #     link = all_a[1]['href']
+    #                 #     img = all_a[0].find('img')['src']
+    #                 #
+    #                 #     if 'https' in link and not "Learn more about Sponsored Products." in title and len(title) > 5:
+    #                 #
+    #                 #         new_item = Item()
+    #                 #         new_item.title = title
+    #                 #         new_item.link = link
+    #                 #         new_item.image = img
+    #                 #         # new_item.rating_count = rating_count
+    #                 #         # new_item.rating = rating
+    #                 #         item_list.append(new_item)
+    #                 # except:
+    #                 #
+    #                 #     pass
+    #
+    #         return  item_list
+
+
+
+
+
+
+
+
+
+
+
 
--- items/views.py
+++ items/views.py
@@ -7,11 +7,14 @@ from .amazon_models import Item
 def item_list(request):
     if request.method == "POST":
         q_word = request.POST['query_word']
+
         if q_word:
             item = Item()
-            search_result = item.get_items(q_word=q_word)
+            search_result = item.get_rectangle_items(q_word=q_word)
+
+
             context = {
-             "item_list": search_result[:10]
+             "item_list": search_result
             }
             return render(request,'items/item_list.html', context)
         else:
