138a7fe344ea051378bb99ebf669d3aacf29a359
Cj <cj@Cjs-MacBook-Pro.local>
2018-06-04 16:12:45 -0700

    add email

--- dev_best10/settings/production.py
+++ dev_best10/settings/production.py
@@ -135,8 +135,10 @@ STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/'
 
 
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_HOST_USER = 'cj160901@gmail.com'
 EMAIL_HOST_PASSWORD = 'luo110015'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'cj160901@gmail.com'
